9bb2681a120ff38d3a7e93ccaff08b6017ba9671
Magnetic_dud <mail@dandandin.it>
2013-12-15 19:35:22 +0100

    Initial commit, it download the average bitcoin price from mtgox.com and shows it on screen. That's it.

---
+++ JsonExample/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Data.Json;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace JsonExample
+{
+    public sealed partial class MainPage : Page
+    {
+        public string myURI = "http://data.mtgox.com/api/1/BTCUSD/ticker";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            GetJSON();
+        }
+
+        private async void GetJSON()
+        {
+            var http = new HttpClient();
+            //http.MaxResponseContentBufferSize = Int32.MaxValue;
+            var response = await http.GetStringAsync(myURI);
+
+            MyClass.RootObject rootObject = JsonConvert.DeserializeObject<MyClass.RootObject>(response);
+
+            //txtBlockResult.Text = rootObject.result; //success
+            txtBlockResult.Text = rootObject.@return.avg.display_short.ToString();
+        }
+    }
+}
---
+++ JsonExample/MyClass.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonExample
+{
+    class MyClass
+    {
+        //generate at http://json2csharp.com/
+        public class High
+        {
+            public string value { get; set; }
+            public string value_int { get; set; }
+            public string display { get; set; }
+            public string display_short { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Low
+        {
+            public string value { get; set; }
+            public string value_int { get; set; }
+            public string display { get; set; }
+            public string display_short { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Avg
+        {
+            public string value { get; set; }
+            public string value_int { get; set; }
+            public string display { get; set; }
+            public string display_short { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Vwap
+        {
+            public string value { get; set; }
+            public string value_int { get; set; }
+            public string display { get; set; }
+            public string display_short { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Vol
+        {
+            public string value { get; set; }
+            public string value_int { get; set; }
+            public string display { get; set; }
+            public string display_short { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class LastLocal
+        {
+            public string value { get; set; }
+            public string value_int { get; set; }
+            public string display { get; set; }
+            public string display_short { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class LastOrig
+        {
+            public string value { get; set; }
+            public string value_int { get; set; }
+            public string display { get; set; }
+            public string display_short { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class LastAll
+        {
+            public string value { get; set; }
+            public string value_int { get; set; }
+            public string display { get; set; }
+            public string display_short { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Last
+        {
+            public string value { get; set; }
+            public string value_int { get; set; }
+            public string display { get; set; }
+            public string display_short { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Buy
+        {
+            public string value { get; set; }
+            public string value_int { get; set; }
+            public string display { get; set; }
+            public string display_short { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Sell
+        {
+            public string value { get; set; }
+            public string value_int { get; set; }
+            public string display { get; set; }
+            public string display_short { get; set; }
+            public string currency { get; set; }
+        }
+
+        public class Return
+        {
+            public High high { get; set; }
+            public Low low { get; set; }
+            public Avg avg { get; set; }
+            public Vwap vwap { get; set; }
+            public Vol vol { get; set; }
+            public LastLocal last_local { get; set; }
+            public LastOrig last_orig { get; set; }
+            public LastAll last_all { get; set; }
+            public Last last { get; set; }
+            public Buy buy { get; set; }
+            public Sell sell { get; set; }
+            public string item { get; set; }
+            public string now { get; set; }
+        }
+
+        public class RootObject
+        {
+            public string result { get; set; }
+            public Return @return { get; set; }
+        }
+    }
+}
